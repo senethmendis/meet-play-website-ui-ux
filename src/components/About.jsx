547c145ae9f834352be99ce80e454aba4e6703ba
wolfy-six <mendis.seneth@gmail.com>
2023-08-12 23:25:21 +0530

    vite config

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styles from "../styles";
 import { aboutBg } from "../assets";
+import {motion as m}from 'framer-motion'
+
 const About = () => {
   return (
     <section
       className={`${styles.flexCenter} rounded-2xl relative flex flex-col bg-about-section`}
     >
-      <div className="absolute text-center">
+      <m.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{delay:0.3}} className="absolute text-center">
         <h1 className={`${styles.headding}`}>Meet Play.</h1>
         <p className={`${styles.paragraph} text-3xl py-6`}>
           Design and build better products, in less time, with fewer barries -
           all <br /> on oyur mobile device.{" "}
         </p>
-      </div>
+      </m.div>
       
       <div className="absolute w-[40%] h-[30%] z-50 trq__gradient  bottom-0"/>
       <img
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,22 +9,22 @@ const Hero = () => {
       <div
         className={`${styles.flexCenter} flex-col flex relative w-full h-full`}
       >
-        <p
+        <m.p initial={{opacity:0,y:-50}} whileInView={{opacity:1,y:0}} transition={{delay:0.1}}
           className={`absolute z-40 top-0 py-4 ${styles.paragraph} text-center text-xl  sm:text-3xl`}
         >
           Can designing on your phone be better than <br /> on your desktop?
-        </p>
+        </m.p>
 
-        <img
+        <m.img initial={{opacity:0,x:50}} whileInView={{opacity:1,x:0}} transition={{delay:0.1}}
           src={iPhone1}
           alt="gradphone"
           className="  w-[350px] absolute z-30"
         />
-        <h1
+        <m.h1 initial={{opacity:0,x:50}} whileInView={{opacity:1,x:0}} transition={{delay:0.9}}
           className={`text-center text-[200px] md:text-[200px] lg:text-[300px] font-bold absolute z-20`}
         >
           Meet Play
-        </h1>
+        </m.h1>
       </div>
       <div className="absolute z-0 blue__gradient w-[30%] h-[20%] right-0" />
       <div className="absolute z-0 trq__gradient w-[30%] h-[20%] mx-auto" />
